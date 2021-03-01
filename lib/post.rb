6b1eaa7c7221cc92c90273125d968044cfdd6309
Barak Rosner <rosnerbb@gmail.com>
2021-02-28 20:27:08 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+
+attr_accessor :title, :author 
+
+    def initialize=(title)
+        @title = title 
+        #@author = author 
+    end
+
+    #def author=(author)
+     #   @author = author
+
+    #end
+
+end
+
+#author = Author.new 
